776cbb92fbefd6c824145848840ff9fb2d2c598f
vjp98982 <89861559+vjp98982@users.noreply.github.com>
2021-10-17 17:44:02 -0400

    Update homework_3.sh
    Updated mummer plot code, attempted to correct canu

--- homework_3.sh
+++ homework_3.sh
@@ -25,26 +25,38 @@ module load QUAST/5.0.2-foss-2019b-Python-3.7.4
 module load MUMmer/3.23_conda
 
 # curl the refseq genome sequence
-curl -s https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz | gunzip -c > ${OUTDIR}/GCF_000005845.2_ASM584v2.fna
+#curl -s https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz | gunzip -c > ${OUTDIR}/GCF_000005845.2_ASM584v2.fna
 
 # download pacbio long reads
-wget -O ${OUTDIR}/pacbio.fq.gz http://bergmanlab.genetics.uga.edu/data/downloads/gene8940/ecoli_p6_25x.filtered.fastq.gz
+#wget -O ${OUTDIR}/pacbio.fq.gz http://bergmanlab.genetics.uga.edu/data/downloads/gene8940/ecoli_p6_25x.filtered.fastq.gz
 
 # dowload illumina short reads
-wget -O ${OUTDIR}/illumina_read1.fq.gz http://bergmanlab.genetics.uga.edu/data/downloads/gene8940/s_6_1.fastq.gz
-wget -O ${OUTDIR}/illumina_read2.fq.gz http://bergmanlab.genetics.uga.edu/data/downloads/gene8940/s_6_2.fastq.gz
+#wget -O ${OUTDIR}/illumina_read1.fq.gz http://bergmanlab.genetics.uga.edu/data/downloads/gene8940/s_6_1.fastq.gz
+#wget -O ${OUTDIR}/illumina_read2.fq.gz http://bergmanlab.genetics.uga.edu/data/downloads/gene8940/s_6_2.fastq.gz
 
 # assemble the e coli MG1655 genome using PacBio long reads with Canu
 canu -p MG1655 -d $OUTDIR genomeSize=4.8m useGrid=false -pacbio-raw pacbio.fq.gz
 
+
+
+canu \
+ -p MG1655 -d $OUTDIR \
+ genomeSize=4.8m \
+ -pacbio-raw pacbio.fq.gz
+
+ 
 # assemble the E coli MG1655 genome using Illumina short read data with SPADES
-spades.py -t 6 -k 21,33,55,77 --isolate --memory 24 --pe1-1 ${OUTDIR}/illumina_read1.fq.gz --pe1-2 ${OUTDIR}/illumina_read2.fq.gz -o ${OUTDIR}
+#spades.py -t 6 -k 21,33,55,77 --isolate --memory 24 --pe1-1 ${OUTDIR}/illumina_read1.fq.gz --pe1-2 ${OUTDIR}/illumina_read2.fq.gz -o ${OUTDIR}
 
 # run QUAST to generate assembly quality assessment statistics
 quast.py -o $OUTDIR -t 6 -r ${OUTDIR}/GCF_000005845.2_ASM584v2.fna ${OUTDIR}/MG1655.contigs.fasta ${OUTDIR}/scaffolds.fasta
 
 # mummer for pacbio
-nucmer ${OUTDIR}/GCF_000005845.2_ASM584v2.fna ${OUTDIR}/MG1655.contigs.fasta -p PacBio_mummer delta-filter -1 PacBio_mummer.delta > PacBio_mummer.1delta mummerplot --size large -layout --color -f --png PacBio_mummer.1delta -p pacbio_mummer
+nucmer ${OUTDIR}/GCF_000005845.2_ASM584v2.fna ${OUTDIR}/MG1655.contigs.fasta -p PacBio_mummer
+delta-filter -1 PacBio_mummer.delta > PacBio_mummer.1delta
+mummerplot --size large -layout --color -f --png illumina_mummer.1delta -p illumina_mummer
 
 # mummer for illumina_read1
-nucmer ${OUTDIR}/GCF_000005845.2_ASM584v2.fna ${OUTDIR}/scaffolds.fasta -p illumina_mummer delta-filter -1 illumina_mummer.delta > illumina_mummer.1delta mummerplot --size large -layout --color -f --png illumina_mummer.1delta -p illumina_mummer
+nucmer ${OUTDIR}/GCF_000005845.2_ASM584v2.fna ${OUTDIR}/scaffolds.fasta -p illumina_mummer
+delta-filter -1 illumina_mummer.delta > illumina_mummer.1delta
+mummerplot --size large -layout --color -f --png illumina_mummer.1delta -p illumina_mummer
