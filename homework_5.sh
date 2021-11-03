811261107bd80622ef36a738d052c0beba1be52c
vjp98982 <89861559+vjp98982@users.noreply.github.com>
2021-11-02 21:05:56 -0400

    Update homework_5.sh

--- homework_5.sh
+++ homework_5.sh
@@ -11,7 +11,6 @@
 
 #set input and output directory variables
 OUTDIR="/work/gene8940/vjp98982/homework_5"
-KALLISTO="$OUTDIR/kallisto"
 
 #if output directory doesn't exist, create it
 if [ ! -d $OUTDIR ]
@@ -25,10 +24,10 @@ curl -s https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2
 module load kallisto/0.46.1-foss-2019b
 kallisto index -i ${OUTDIR}/GCF_000005845.2_ASM584v2_cds.fa.idx ${OUTDIR}/GCF_000005845.2_ASM584v2_cds.fa
 
-kallisto quant -t 6 -b 100 -i ${OUTDIR}/GCF_000005845.2_ASM584v2_cds.fa.idx -o ${KALLISTO}/SRR5344681 /work/gene8940/instructor_data/SRR5344681_1.fastq.gz /work/gene8940/instructor_data/SRR5344681_2.fastq.gz
-kallisto quant -t 6 -b 100 -i ${OUTDIR}/GCF_000005845.2_ASM584v2_cds.fa.idx -o ${KALLISTO}/SRR5344682 /work/gene8940/instructor_data/SRR5344682_1.fastq.gz /work/gene8940/instructor_data/SRR5344682_2.fastq.gz
-kallisto quant -t 6 -b 100 -i ${OUTDIR}/GCF_000005845.2_ASM584v2_cds.fa.idx -o ${KALLISTO}/SRR5344683 /work/gene8940/instructor_data/SRR5344683_1.fastq.gz /work/gene8940/instructor_data/SRR5344683_2.fastq.gz
-kallisto quant -t 6 -b 100 -i ${OUTDIR}/GCF_000005845.2_ASM584v2_cds.fa.idx -o ${KALLISTO}/SRR5344684 /work/gene8940/instructor_data/SRR5344684_1.fastq.gz /work/gene8940/instructor_data/SRR5344684_2.fastq.gz
+kallisto quant -t 6 -b 100 -i ${OUTDIR}/GCF_000005845.2_ASM584v2_cds.fa.idx -o ${OUTDIR}/kallisto/SRR5344681 /work/gene8940/instructor_data/SRR5344681_1.fastq.gz /work/gene8940/instructor_data/SRR5344681_2.fastq.gz
+kallisto quant -t 6 -b 100 -i ${OUTDIR}/GCF_000005845.2_ASM584v2_cds.fa.idx -o ${OUTDIR}/kallisto/SRR5344682 /work/gene8940/instructor_data/SRR5344682_1.fastq.gz /work/gene8940/instructor_data/SRR5344682_2.fastq.gz
+kallisto quant -t 6 -b 100 -i ${OUTDIR}/GCF_000005845.2_ASM584v2_cds.fa.idx -o ${OUTDIR}/kallisto/SRR5344683 /work/gene8940/instructor_data/SRR5344683_1.fastq.gz /work/gene8940/instructor_data/SRR5344683_2.fastq.gz
+kallisto quant -t 6 -b 100 -i ${OUTDIR}/GCF_000005845.2_ASM584v2_cds.fa.idx -o ${OUTDIR}/kallisto/SRR5344684 /work/gene8940/instructor_data/SRR5344684_1.fastq.gz /work/gene8940/instructor_data/SRR5344684_2.fastq.gz
 
 
 conda activate R
