6d4f3710c4bd1407adb4f4269d7e82b939dfd9a1
vjp98982 <89861559+vjp98982@users.noreply.github.com>
2021-12-06 22:20:06 -0500

    Update project_fullasm.sh
    updated final script

--- project_fullasm.sh
+++ project_fullasm.sh
@@ -48,52 +48,35 @@ then
     mkdir -p ${OUTDIR}/quast/find_genes
 fi
 
-
-
 module load SRA-Toolkit/2.9.6-1-centos_linux64
 module load FastQC/0.11.9-Java-11
-module load Trim_Galore/0.6.5-GCCcore-8.3.0-Java-11-Python-3.7.4
 module load SPAdes/3.14.1-GCC-8.3.0-Python-3.7.4
-module load  seqtk/1.3-GCC-8.3.0
+module load seqtk/1.3-GCC-8.3.0
 module load QUAST/5.0.2-foss-2019b-Python-3.7.4
 module load MUMmer/3.23_conda
-
 
 
 # retrieve existing illumina-based assembly from a different dataset
 curl -s https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/003/313/985/GCA_003313985.1_ASM331398v1/GCA_003313985.1_ASM331398v1_genomic.fna.gz | gunzip -c > ${OUTDIR}/GCA_003313985.1_ASM331398v1_genomic.fna
 
 # dowload and extract paired end illumina reads from ncbi for Lactarius indigo SRA file for run SRR5804120
-#prefetch -O ${OUTDIR} SRR5804120
-#fastq-dump --split-files --gzip ${OUTDIR}/SRR5804120.sra -O ${OUTDIR}
+prefetch -O ${OUTDIR} SRR5804120
+fastq-dump --split-files --gzip ${OUTDIR}/SRR5804120.sra -O ${OUTDIR}
 
 # dowload and extract paired end illumina reads from ncbi for Lactarius indigo SRA file for run SRR5804121
-#prefetch -O ${OUTDIR} SRR5804121
-#fastq-dump --split-files --gzip ${OUTDIR}/SRR5804121.sra -O ${OUTDIR}
+prefetch -O ${OUTDIR} SRR5804121
+fastq-dump --split-files --gzip ${OUTDIR}/SRR5804121.sra -O ${OUTDIR}
 
 # perform fastqc to assess read quality
-#fastqc ${OUTDIR}/SRR5804120_1.fastq.gz ${OUTDIR}/SRR5804120_2.fastq.gz ${OUTDIR}/SRR5804121_1.fastq.gz ${OUTDIR}/SRR5804121_2.fastq.gz --outdir ${OUTDIR}/fastqc_out
+fastqc ${OUTDIR}/SRR5804120_1.fastq.gz ${OUTDIR}/SRR5804120_2.fastq.gz ${OUTDIR}/SRR5804121_1.fastq.gz ${OUTDIR}/SRR5804121_2.fastq.gz --outdir ${OUTDIR}/fastqc_out
 
-# use trimgalore to improve the quality of the reads prior to spades assembly. Trim out adapter sequences
+# assemble using spades
+spades.py -t 10 -k 21,33,55,77 --isolate --memory 128 --pe1-1 ${SRADIR}/SRR5804120_1.fastq.gz --pe1-2 ${SRADIR}/SRR5804120_2.fastq.gz  -o ${OUTDIR}/untrimmed_SRR5804120
 
-##### this is the assembly command from HW3, use as a model for the project
-# assemble the E coli MG1655 genome using Illumina short read data with SPADES
-#spades.py -t 8 -k 21,33,55,77 --isolate --memory 32 --pe1-1 ${OUTDIR}/illumina_read1.fastq.gz --pe1-2 ${OUTDIR}/illumina_read2.fastq.gz -o ${OUTDIR}
+# quast to assess assembly & testing quast for gene prediction
+quast.py --fungus --glimmer -o ${OUTDIR}/quast/find_genes -t 10 ${OUTDIR}/untrimmed_SRR5804120/scaffolds.fasta
 
-# removed "--pe2-1 ${OUTDIR}/SRR5804121_1.fastq.gz --pe2-2 ${OUTDIR}/SRR5804121_2.fastq.gz"
-# running out of memory and that replicate is far less clean
-#spades.py -t 10 -k 21,33,55,77 --isolate --memory 128 --pe1-1 ${SRADIR}/SRR5804120_1.fastq.gz --pe1-2 ${SRADIR}/SRR5804120_2.fastq.gz  -o ${OUTDIR}/untrimmed_SRR5804120
-
-# quast to assess assembly
-#quast.py --fungus -o ${OUTDIR}/sub_quast -t 10 ${OUTDIR}/untrimmed_SRR5804120/scaffolds.fasta
-
-# testing quast for gene prediction
-#quast.py --fungus --gene-finding -o ${OUTDIR}/sub_quast/find_genes -t 10 ${OUTDIR}/untrimmed_SRR5804120/scaffolds.fasta
-
-# testing quast for gene prediction
-#quast.py --fungus --glimmer -o ${OUTDIR}/quast/find_genes -t 10 ${OUTDIR}/untrimmed_SRR5804120/scaffolds.fasta
-
-# compare my assembly to that of another experiment on the same mushroom species
+# compare my assembly to that of the published assembly
 nucmer ${OUTDIR}/GCA_003313985.1_ASM331398v1_genomic.fna ${OUTDIR}/untrimmed_SRR5804120/scaffolds.fasta -p illumina_mummer
 delta-filter -i 80 -l 1000 -1 illumina_mummer.delta > illumina_mummer.1delta
 mummerplot --size large -layout --color -f --png illumina_mummer.1delta -p illumina_mummer
